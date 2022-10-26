9981186fe762a46d2fd98bf3e253b481840fbf47
Justyn Hess <justynhess7789@gmail.comm>
2022-10-26 11:30:58 -0400

    intro

---
+++ animal.py
@@ -0,0 +1,15 @@
+class animal():
+
+    def __innit__(self):
+        self.type = ""
+        self.size = 0
+        self.age = 0
+        self.name = ""
+
+    def speak(self):
+        pass
+
+    def describe(self):
+        pass
+
+    
