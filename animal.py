3794edd28ccad245fedc4607442f94bdf85074b5
Justyn Hess <112112528+j5677@users.noreply.github.com>
2022-11-06 18:07:13 -0500

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,15 +1,34 @@
-class animal():
+class Animal():
 
-    def __innit__(self):
-        self.type = ""
+    def __init__(self, type):
+        self.type = type
         self.size = ""
         self.age = 0
-        self.name = ""
+        if type == "cat" :
+            self.name = "Seymour"
+        else :
+            self.name = "Spot"
+        
 
     def speak(self):
-        pass
+        if self.type == "cat" :
+            if self.size == "small" :
+                return "meow"
+            else :
+                return "MEOW!"   
+        else :
+            if self.size == "small" :
+                return "bow wow"
+            if self.size == "medium" :
+                return "Ruff ruff"
+            else :
+                return "arf arf"
+            
 
     def describe(self):
-        pass
+        if self.age >= 10 :
+            return self.name + " is old"
+        else :
+            return self.name + " is young"
 
     
